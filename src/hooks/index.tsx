6946c554d0c59d262f6c3f62164bd21e1b900f9a
NgoCuong <vj3tcu0ng1998@gmail.com>
2025-10-28 10:00:10 +0700

    format code

--- src/hooks/index.tsx
+++
@@ -1,4 +0,0 @@
-import { useComposable } from "./useComposable";
-import { useFetchApi } from "./useFetchApi";
-
-export { useComposable, useFetchApi };
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {CoSoNuoiYen} from "./pages/CoSoNuoiYen.tsx";
 import { DataGridComponent } from "./components/DataGridComponent.tsx";
-import TheoDoiCongTacKDDV from "./pages/TheoDoiCongTacKDDV.tsx";
-import TheoDoiCongTacKDDVForm from "./pages/TheoDoiCongTacKDDVForm.tsx";
+import { TheoDoiCongTacKDDV } from "./pages/TheoDoiCongTacKDDV.tsx";
 
-export { React, ReactDOM, DataGridComponent, CoSoNuoiYen , TheoDoiCongTacKDDV,TheoDoiCongTacKDDVForm };
+export { React, ReactDOM, DataGridComponent, CoSoNuoiYen , TheoDoiCongTacKDDV };
