d3d2421a557ede609482380c31675741074ae572
Nguyen Loc <nguyenloc@vnpt.vn>
2025-10-28 09:14:44 +0700

    update

--- src/features/PlantVarietiesManagement/ProductionBusinessManagement/ProductionBusinessMasterDetail.tsx
+++ src/features/PlantVarietiesManagement/ProductionBusinessManagement/ProductionBusinessMasterDetail.tsx
@@ -1,53 +1,10 @@
-import {DateBox, SelectBox, TextBox} from "devextreme-react";
-import {Button} from "devextreme-react/button";
-import {Controller, useFieldArray, useForm} from "react-hook-form";
 import DataGridTable from "../../../core/components/DataGridTable/DataGridTable.tsx";
-
-const coQuanOptions = [
-    {id: "75", name: "Chi cục Chăn nuôi và Thú y"},
-    {id: "348", name: "Cục thú y huyện Chợ Gạo"},
-    {id: "465", name: "Sở Nông nghiệp và Phát triển NT Hà Nam"},
-    {id: "496", name: "Sở Nông nghiệp và Phát triển NT Thanh Hóa"},
-    {id: "497", name: "Chi cục chăn nuôi và thú y "},
-    {id: "619", name: "Sở Nông Nghiệp và Phát Triển Nông Thôn tỉnh Ninh Bình"},
-    {id: "737", name: "Chi cục Thú Y"},
-    {id: "752", name: "Sở NNPTNT YBI"},
-    {id: "938", name: "a"},
-    {id: "939", name: "b"},
-    {id: "940", name: "c"},
-    {id: "1065", name: "TEST XÓA"},
-];
-
-const hinhThucOptions = [
-    {id: "81", name: "Cấp mới"},
-    {id: "358", name: "Cấp lại"},
-    {id: "427", name: "Giấy chứng nhận ATTP"},
-    {id: "485", name: "Lê Test"},
-    {id: "617", name: "a"},
-    {id: "692", name: "Loại cấp mới"},
-    {id: "728", name: "Loại1"},
-    {id: "1044", name: "a"},
-    {id: "1045", name: "b"},
-    {id: "1046", name: "c"},
-    {id: "1047", name: "d"},
-    {id: "1067", name: "TEST XÓA"},
-];
 
 export const ProductionBusinessMasterDetail = ({runQuery, model, updateModel}: {
     runQuery: () => void;
     model: any;
     updateModel: () => void
 }) => {
-    const {control} = useForm<any>({
-        defaultValues: {
-            dataTable: model?.dataTable,
-        },
-    });
-
-    const {fields, append, remove} = useFieldArray({
-        control,
-        name: "dataTable",
-    });
 
     const columns = [
         {
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,8 @@ import ReactDOM from "react-dom";
 import {
     ProductionBusinessTable
 } from "./features/PlantVarietiesManagement/ProductionBusinessManagement/ProductionBusinessTable.tsx";
+import {
+    ProductionBusinessMasterDetail
+} from "./features/PlantVarietiesManagement/ProductionBusinessManagement/ProductionBusinessMasterDetail.tsx";
 
-export {React, ReactDOM, ProductionBusinessTable};
+export {React, ReactDOM, ProductionBusinessTable, ProductionBusinessMasterDetail};
